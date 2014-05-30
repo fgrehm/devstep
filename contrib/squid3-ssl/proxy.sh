19909547a114490ff3ce4c4f5cee5e0c4f4be18b
Fabio Rehm <fgrehm@gmail.com>
2014-05-29 23:57:00 -0300

    contrib/squid3-ssl: Make sure nodejs / npm doesnt complain about our hand made certificates

--- contrib/squid3-ssl/proxy.sh
+++ contrib/squid3-ssl/proxy.sh
@@ -7,3 +7,9 @@ if ! [ -z "${HTTPS_PROXY_CERT}" ]; then
   sudo sh -c "echo '$(basename ${HTTPS_PROXY_CERT})' >> /etc/ca-certificates.conf"
   sudo /usr/sbin/update-ca-certificates
 fi
+
+if $(which npm &>/dev/null); then
+  npm config set strict-ssl false
+elif ! [ -f $HOME/.npmrc ] || ! $(grep -q 'strict-ssl' $HOME/.npmrc); then
+  echo 'strict-ssl = false' >> $HOME/.npmrc
+fi
