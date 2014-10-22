6a1ccaa1a35763067e19be4480557ce032867430
Fabio Rehm <fgrehm@gmail.com>
2014-10-22 02:44:26 -0200

    baseimage: Progress on improving organization within the image [GH-63]

--- stack/load-env.sh
+++ stack/load-env.sh
@@ -7,15 +7,16 @@ alias la='ls -A'
 alias l='ls -CF'
 
 # shortcut for reloading envs after a build
-alias reload-env="source ${HOME}/load-env.sh"
+alias reload-env="source /opt/devstep/load-env.sh"
 
 # shorten PS1 (useful for golang projects)
 export PROMPT_DIRTRIM="2"
 
-export PATH="/.devstep/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export DEVSTEP_BIN="/opt/devstep/bin"
+export PATH="${HOME}/bin:${DEVSTEP_BIN}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # TODO: Review these paths
-profile_dirs="/etc /.devstep ${PWD}/.devstep"
+profile_dirs="/etc ${HOME}"
 for dir in $profile_dirs; do
   if [ -d "${dir}/.profile.d" ]; then
     for i in "${dir}/.profile.d/"*.sh; do
